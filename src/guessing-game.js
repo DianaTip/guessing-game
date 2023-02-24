0b736b80ddfd98bf8cca5ae041c9547a1f281f63
Diana <dianatipunova@gmail.com>
2023-02-24 12:44:43 +0300

    feat: update

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,43 +1,24 @@
 class GuessingGame {
-    constructor() {
-        }
-
-   /* constructor(number) {
+      constructor(number) {
         this.number = number;
-      }*/
+      }
 
     setRange(min, max) {
-     /*   const game = new GuessingGame();
-        game.setRange(min, max)*/
      this.min = min;
      this.max = max;   
     }
 
     guess() {
-        return this.middle = Math.floor((this.max+this.min)/2);
-       /* this.middle = Math.floor((this.max+this.min)/2);
-        if (this.number === this.middle)
-           {return this.middle}
-        else
-          {if (this.number < this.middle)
-               {return this.greater()}
-           else
-               {return this.lower()}
-           }*/
-    }
+       return  this.middle = Math.round((this.max+this.min)/2);
+         }
 
     lower() {
-      return  this.min = this.middle;
-     /*   return this.guess();*/
+     return this.max = this.middle;
     }
 
     greater() {
-       return this.max = this.middle;
-     /*  return this.guess();*/
+     return this.min = this.middle;
     }
 }
-/*const game = new GuessingGame ();
-game.setRange();
-game.number*/
 
 module.exports = GuessingGame;
